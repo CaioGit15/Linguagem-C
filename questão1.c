6aab230da58d8aa04f47edca2c5f44282ef74615
CaioGit15 <164903692+CaioGit15@users.noreply.github.com>
2025-05-29 20:21:05 -0300

    Create questão1.c

---
+++ questão1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+int n1, n2, soma;
+printf("Vamos somar dois números!");
+printf("\nDigite o primeiro número:");
+scanf("%d", &n1);
+printf("Digite o segundo número:");
+scanf("%d", &n2);
+
+soma = n1 + n2;{
+printf("A soma dos dois números é %d", soma);
+}
+    return 0;
+}

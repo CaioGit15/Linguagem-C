7217de5ff97f1282bd31a4d02214cf362acf8a98
CaioGit15 <164903692+CaioGit15@users.noreply.github.com>
2025-06-04 11:10:45 -0300

    Update questão34.c

--- questão34.c
+++ questão34.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-// Nesse código estudei comandos por fora!
+
 int main() {
-char nome[100], endereco[100], telefone[30];
-int idade;
-printf("Digite seu nome:");
-fgets(nome, sizeof(nome), stdin);
-printf("Digite seu endereço:");
-fgets(endereco, sizeof(endereco), stdin);
-printf("Digite seu telefone:");
-fgets(telefone, sizeof(telefone), stdin);
-printf("Digite sua idade:");
-scanf("%d", &idade);
-printf("O seu nome é %s, o seu endereço é %s, seu telefone é %s, e sua idade é %d", nome, endereco, telefone, idade);
+    char nome[100], endereco[100], telefone[30];
+    int idade;
+
+    printf("Digite seu nome: ");
+    scanf("%s", nome);
+
+    printf("Digite seu endereço: ");
+    scanf("%s", endereco);
+
+    printf("Digite seu telefone: ");
+    scanf("%s", telefone);
+
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    printf("O seu nome é %s, o seu endereço é %s, seu telefone é %s, e sua idade é %d\n",
+           nome, endereco, telefone, idade);
+
     return 0;
-   
 }
